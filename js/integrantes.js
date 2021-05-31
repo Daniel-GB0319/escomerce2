fbe6656a47ea71147114da52eeae7905361ebe92
Manuel Ortiz <manuel4612@hotmail.com>
2021-05-31 18:28:42 -0500

    Primera union Reparacion de integrantes

--- js/integrantes.js
+++ js/integrantes.js
@@ -10,7 +10,7 @@ let id = '';
 const saveIntegrantes = (url_foto, nombre_integrante, escuela_integrante, desc_integrante, social_integrante) =>
     //Creará la coleccion de la base de datos en Firebase
     //aquí se pondrá el nombre de cada entidad(si no existe, Firebase la creará en automático)
-    db.collection('integrantes').doc().set({
+    db.collection('clientes').doc().set({
         url_foto,
         nombre_integrante,
         escuela_integrante,
@@ -18,12 +18,12 @@ const saveIntegrantes = (url_foto, nombre_integrante, escuela_integrante, desc_i
         social_integrante
     })
 //Funcion para imprimir la informacion
-const getIntegrantes = () => db.collection('integrantes').get();
-const getIntegrante = (id) => db.collection('integrantes').doc(id).get();
-const onGetIntegrantes = (callback) => db.collection('integrantes').onSnapshot(callback);
-const deleteIntegrante = (id) => db.collection('integrantes').doc(id).delete();
-const editIntegrante = (id) => db.collection('integrantes').doc(id).get();
-const updateIntegrante = (id, updatedIntegrante) => db.collection('integrantes').doc(id).update(updatedIntegrante);
+const getIntegrantes = () => db.collection('clientes').get();
+const getIntegrante = (id) => db.collection('clientes').doc(id).get();
+const onGetIntegrantes = (callback) => db.collection('clientes').onSnapshot(callback);
+const deleteIntegrante = (id) => db.collection('clientes').doc(id).delete();
+const editIntegrante = (id) => db.collection('clientes').doc(id).get();
+const updateIntegrante = (id, updatedIntegrante) => db.collection('clientes').doc(id).update(updatedIntegrante);
 
 //Imprimir
 window.addEventListener('DOMContentLoaded', async (e) => {
@@ -34,50 +34,17 @@ window.addEventListener('DOMContentLoaded', async (e) => {
         //Imprimimos los datos guardados en FireBase en la consola
         querySnapshot.forEach(doc => {
 
-            const clientesDato = doc.data()
-            clientesDato.id = doc.id;
+            const integranteDato = doc.data()
+            integranteDato.id = doc.id;
 
             //Genera un html
-            taskContainer.innerHTML += '<div class="mb-3">
-            <label class="form-label" for="name">Nombre</label><input class="form-control item" type="text" id="nombre_usa12" placeholder="Nombre(s)" required="true"></div>
-        <div class="mb-3">
-            <label class="form-label" for="password">Apellidos</label>
-            <input class="form-control" id="a_paterno12" type="text" placeholder="A. Paterno" required="true">
-            <input class="form-control" id="a_materno12" type="text" placeholder="A. Materno" required="true" style="margin-top: 10px;"></div>
-        <div class="mb-3" id="no_boleta_empleado12">
-            <label class="form-label" for="email">Boleta</label><input class="form-control" type="number" placeholder="No. Boleta o de empleado" min="2" max="19999999999999"></div>
-        <div class="mb-3" id="correo_cuenta12">
-            <label class="form-label" for="email">Correo</label><input class="form-control" type="email" placeholder="Correo institucional" required="true"></div>
-        <div class="mb-3" id="contraseña_cuenta12">
-            <label class="form-label" for="email">Contraseña</label><input class="form-control" type="password" placeholder="Contraseña de 8 a 16 caracteres" required="true"></div>
-        <div class="mb-3" id="contraseña_cuenta42">
-            <label class="form-label" for="email">Repite la contraseña</label><input class="form-control" type="password" placeholder="Contraseña de 8 a 16 caracteres" required="true"></div>
-        <div class="mb-3" id="escuela_procedencia12">
-            <label class="form-label" for="email">Escuela</label>
-            <select class="form-select" required="true">
-                <option value="12" selected="">Escuela de procedencia</option>
-                <option value="13">ESCOM</option>
-                <option value="14">ESIME Z</option>
-            </select></div>
-        <div class="mb-3">
-            <label class="form-label" for="email">Teléfono</label>
-            <input class="form-control" id="telefono_10dig12" type="tel" placeholder="10 dígitos (opcional)"></div>
-        <div class="mb-3">
-            <div class="form-check">
-                <input class="form-check-input" id="vendedor12" type="checkbox">
-                <label class="form-check-label" for="formCheck-1">Quiero ser vendedor</label>
-            </div>
-        </div>
-        <div class="d-block mb-3">
-            <label class="form-label" for="email">INE</label>
-            <input class="form-control" id="ine12" type="file">
-        </div>
-        <div class="d-block mb-3">
-            <label class="form-label" for="email">Cuenta CLABE</label>
-            <input class="form-control" id="cuenta_clabe12" type="text" placeholder="No. cuenta CLABE">
-        </div>
-        <button class="btn btn-primary" id="actualizar_datos" type="submit" style="background: rgb(52, 73, 94);border-color: rgb(52, 73, 94);border-top-color: rgb(52,border-right-color 73,border-bottom-color 94);border-left-color: 73,;"
-        onclick="inicio()">Actualizar datos</button>>'
+            taskContainer.innerHTML += '<div class="tarjeta_Integrante"><div class="img_Integrante"><img src="' + integranteDato.url_foto +
+                '" alt="' + integranteDato.url_foto +
+                '"></div><h3>' + integranteDato.nombre_integrante +
+                '</h3><div class="desc_Integrante"><h4>' + integranteDato.escuela_integrante +
+                '</h4><p>' + integranteDato.desc_integrante +
+                '</p><a href="#"><p>' + integranteDato.social_integrante +
+                '</p></a></div> <div class="botones_carta"><button class="btn btn-primary btn-delete" data-id="' + integranteDato.id + '">Eliminar</button><button class="btn btn-secundary btn-edit" data-id="' + integranteDato.id + '">Editar</button></div></div>';
 
             const btnDelete = document.querySelectorAll('.btn-delete');
             //console.log(btnDelete)
@@ -93,8 +60,7 @@ window.addEventListener('DOMContentLoaded', async (e) => {
                 btn.addEventListener('click', async (e) => {
                     const doc = await getIntegrante(e.target.dataset.id);
                     const integrante = doc.data();
-                    console.log(e.target.dataset.id)
-                    
+
                     editStatus = true;
                     id = doc.id;
 
