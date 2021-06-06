00760546e26aa22cbad042afb3429dd00e2b4c53
Manuel Ortiz <manuel4612@hotmail.com>
2021-06-06 00:12:09 -0500

    Catalogo Carrito y pago OK Agregar la tarjeta a pedidos

--- js/pedidos.js
+++ js/pedidos.js
@@ -7,6 +7,7 @@ const taskForm = document.getElementById('form_metodoPago');
 let carritoOn = false;
 let idCarritoBuscar = ''
 let productosConfirmados = [];
+let idUsuario = ''
 //Funcion para imprimir la informacion
 const onGetCarrito = (callback) => db.collection('carrito').onSnapshot(callback);
 const onGetPedido = (callback) => db.collection('Confirmar_Pedido').onSnapshot(callback);
@@ -29,31 +30,36 @@ window.addEventListener('DOMContentLoaded', async (e) => {
         querySnapshot.forEach(doc => {
             const consultaCarrito_Pedido = doc.data();
             consultaCarrito_Pedido.id = doc.id;
-            //console.log("ID Carrito:"+consultaCarrito_Pedido.idCarritoFinal)
+            //console.log("ID Carrito:"+consultaCarrito_Pedido.id)
             //console.log("ID Pedido:"+consultaCarrito_Pedido.idPedido)
             console.log("ID: "+consultaCarrito_Pedido.id)
             idCarritoBuscar = consultaCarrito_Pedido.id;
-
+            
         });
+        
         db.collection('Carrito_pedido').where('idCarrito', '==', idCarritoBuscar)
             .get().then((querySnapshot) => {
+                
                 imprPedido.innerHTML = ''
                 totalPedido.innerHTML = '';
 
                 querySnapshot.forEach((doc) => {
+                    
                     const datosPedido = doc.data();
                     datosPedido.id = doc.id;
                     const pedidoConfirmado = []
-                    let idUsuario = ''
+                    idUsuario = datosPedido.idCliente;
+                        console.log(datosPedido.idCliente)
                     //console.log(doc.id, "=>", doc.data());
                     datosPedido.infoPedido.forEach(datos => {
+                        
                         imprPedido.innerHTML += `
                             <div class="item"><span class="price">$ ${datos.costo_producto}</span>
                                 <p class="item-name">${datos.nombre_prod}</p>
                                 <p class="item-description">Descripcion producto: ${datos.descripcion_prod}</p>
                             </div>`;
 
-                        idUsuario = datos.id_cliente;
+                        
                         pedidoConfirmado.push(
                             {
                                 id_producto: datos.id_producto,
@@ -63,16 +69,19 @@ window.addEventListener('DOMContentLoaded', async (e) => {
                                 cantidad_prod: datos.cantidad_prod,
                                 costo_producto: datos.costo_producto,
                             });
+                            
                     })
                     totalPedido.innerHTML += `<span>Total</span><span class="price">$ ${datosPedido.total_pagado}</span>`;
 
                     console.log(pedidoConfirmado)
                     taskForm.addEventListener('submit', async (e) => {
                         
-                        await addPedido(datosPedido.id,idUsuario, pedidoConfirmado, datosPedido.total_pagado);
+                        console.log(datosPedido.id,idUsuario, pedidoConfirmado, datosPedido.total_pagado);
                         
+                        await addPedido(datosPedido.id,idUsuario, pedidoConfirmado, datosPedido.total_pagado);
+                        console.log("Enviado")
                         function redireccionar() { location.href = "catalogo.html"; }
-                        setTimeout(redireccionar(), 25000);
+                        //setTimeout(redireccionar(), 25000);
                     })
 
                 })
@@ -81,7 +90,6 @@ window.addEventListener('DOMContentLoaded', async (e) => {
                 console.log("Error getting documents:", error)
             })
         //console.log("Tengo el ID: " + idCarritoBuscar)
-        pedidoConfirmado = []
     })
 })
 
